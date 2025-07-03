17b1f77d61e62a5fa480adbba88133347856afaa
Pedro Paulo da Silva Monteiro <109928508+PedroPaulo-Git@users.noreply.github.com>
2025-07-03 03:17:06 -0300

    feat(auth): implementação de login e registro com JWT e bcrypt

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,17 +1,17 @@
 import express,{Request,Response} from "express";
 import cors from "cors";
 import 'dotenv/config'
+import authRoutes from "./routes/authRoutes";
 
 const app = express();
 const PORT = process.env.PORT || 5000
 
-console.log(PORT)
 app.use(express.json());
 app.use(cors());
 
-app.get('/',(req:Request,res:Response) => {
-    res.send('Hello world')
-});
+
+app.use("/auth", authRoutes);
+
 
 app.listen(PORT,()=>{
     console.log('Server rodando na porta :',PORT)
--- backend/src/services/authService.ts
+++ backend/src/services/authService.ts
@@ -1,3 +1,31 @@
-// import bcrypt from 'bcrypt'
-// import jwt from 'jsonwebtoken'
-// import prisma from '@prisma/client'
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+export async function registerUser({ email, password }: { email: string, password: string }) {
+    const existingUser = await prisma.user.findUnique({ where: { email } });
+    if (existingUser) throw new Error("Usuário já existe no sistema");
+
+    const hashPassword = await bcrypt.hash(password, 10);
+
+    const user = await prisma.user.create({
+        data: {
+            email,
+            password: hashPassword,
+        }
+    })
+    return { id: user.id, email: user.email }
+}
+export async function loginUser({ email, password }: { email: string, password: string }) {
+    const user = await prisma.user.findUnique({ where: { email } });
+    if (!user) throw new Error(" Usuário não encontrado ")
+    const passwordMatch = await bcrypt.compare(password, user.password);
+    if (!passwordMatch) throw new Error("senha inválida");
+
+    const token = jwt.sign({ id: user.id, email: user.email }, process.env.JWT_SECRET!, {
+        expiresIn: '1d',
+    });
+    return token;
+}
